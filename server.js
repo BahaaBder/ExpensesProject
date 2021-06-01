07e7c09468c64b8d65fc73050c96ee7b82396cfa
bahaabder <bahaa.bder97@gmail.com>
2021-06-01 14:02:57 +0300

    add comment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
 
-
+//parser
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
